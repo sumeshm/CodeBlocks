2ca5a09962e64d92dba7436d9d0e61baa9e5b1e0
Mani <summani@lm-bng-22004269.paypalcorp.com>
2018-01-04 12:55:40 +0530

    Created spring boot project

--- LearnSpringBoot/src/main/java/com/learn/spring4/controller/ServletController.java
+++ LearnSpringBoot/src/main/java/com/learn/spring4/controller/ServletController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,23 +19,25 @@ import org.springframework.web.bind.annotation.RestController;
 import com.learn.spring4.dao.CarDAO;
 import com.learn.spring4.service.CarPojo;
 
+//@Controller
 @RestController
+@RequestMapping("/LearnSpringBoot")
 public class ServletController {
 
-	// http://localhost:8080/LearnSpring4RestSvc/cars/
+	// http://localhost:8080/LearnSpringBoot/cars/
 	
 	private Logger logger = Logger.getLogger(ServletController.class);
 
 	@Autowired
 	private CarDAO carDao;
 
-	@RequestMapping("/cars")
+	@RequestMapping(path = "/cars", produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<CarPojo> getCars() {
 		logger.info("sumesh: Get the list of cars");
 		return carDao.getList();
 	}
 
-	@GetMapping("/cars/{name}")
+	@GetMapping(path = "/cars/{name}", produces = MediaType.APPLICATION_JSON_VALUE)
 	public CarPojo getCar(@PathVariable String name) {
 		CarPojo car = carDao.get(name);
 		if (car != null)
@@ -45,12 +48,12 @@ public class ServletController {
 		return (new CarPojo());
 	}
 
-	@PostMapping(path = "/cars", consumes = "application/json")
+	@PostMapping(path = "/cars", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<CarPojo> createCar(@RequestBody CarPojo car) {
 		return new ResponseEntity<CarPojo>(carDao.create(car), HttpStatus.OK);
 	}
 
-	@PutMapping(path = "/cars/{name}", consumes = "application/json")
+	@PutMapping(path = "/cars/{name}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<CarPojo> updateCar(@PathVariable String name, @RequestBody CarPojo car) {
 		
 		CarPojo updatedCar = carDao.update(name, car);
